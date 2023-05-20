822710581f0e4171fa483288ca304f9d20b0e32f
Merve <mervegundogmus@outlook.com>
2023-05-21 01:26:15 +0300

    Repository created

---
+++ spring-reservation-demo/src/main/java/com/mgundogmus/springreservationdemo/data/RoomRepository.java
@@ -0,0 +1,8 @@
+package com.mgundogmus.springreservationdemo.data;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends CrudRepository<Room, Long> {
+}
