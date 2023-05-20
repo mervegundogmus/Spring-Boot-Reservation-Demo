3212c39d4a79d4599bb3006e00bbe17c2401dac9
Merve <mervegundogmus@outlook.com>
2023-05-21 00:05:05 +0300

    Initial commit

---
+++ spring-reservation-demo/src/main/java/com/mgundogmus/springreservationdemo/SpringReservationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mgundogmus.springreservationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReservationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReservationDemoApplication.class, args);
+	}
+
+}
