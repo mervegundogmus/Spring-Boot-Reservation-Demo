60101fce876f192dca29e04d7841e646d0657546
Merve <mervegundogmus@outlook.com>
2023-05-22 00:13:01 +0300

    Guest and reservation created

---
+++ spring-reservation-demo/src/main/java/com/mgundogmus/springreservationdemo/data/GuestRepository.java
@@ -0,0 +1,8 @@
+package com.mgundogmus.springreservationdemo.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuestRepository extends JpaRepository<Guest, Long> {
+}
